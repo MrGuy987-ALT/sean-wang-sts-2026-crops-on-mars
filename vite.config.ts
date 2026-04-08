4ddbace4ee1e3d653f98f83c907ae3c0ba85ddb7
MrGuy987 Alt <2030swang@gmail.com>
2026-04-08 22:52:13 +0000

    disable source maps and add vite env types

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/sean-wang-sts-2026-crops-on-mars/',
+  build: {
+    sourcemap: false,
+  },
 })
